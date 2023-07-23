87271362e891cbed01e6243bce08141c69106365
Masashi Sato <gmasa.310@gmail.com>
2023-07-23 16:02:22 +0900

    update

--- .wezterm.lua
+++ .wezterm.lua
@@ -15,7 +15,7 @@ config.color_scheme = 'Molokai'
 config.window_background_opacity = 0.95
 config.front_end = "OpenGL"
 config.font = wezterm.font 'Inconsolata Nerd Font Mono'
-config.font_size = 11.0
+config.font_size = 13.0
 config.hide_tab_bar_if_only_one_tab = true
 
 

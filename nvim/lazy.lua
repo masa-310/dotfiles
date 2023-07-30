7a055462752eb8164e6d692750141a9df67edca4
Masashi Sato <gmasa.310@gmail.com>
2023-07-30 21:37:19 +0900

    update

--- nvim/lazy.lua
+++ nvim/lazy.lua
@@ -156,11 +156,11 @@ require("lazy").setup({
           -- markdown = {'markdownlint'},
           -- nix = {'nix'},
         }
-        --vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-        --  callback = function()
-        --    require("lint").try_lint()
-        --  end,
-        --})
+        vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+          callback = function()
+            require("lint").try_lint()
+          end,
+        })
       end
     },
     {"mhartington/formatter.nvim",
@@ -186,8 +186,7 @@ require("lazy").setup({
                 function()
                   return {
                     exe = "gofmt",
-                    args = {"-w", util.escape_path(util.get_current_buffer_file_path())},
-                    stdin = false
+                    stdin = true
                   }
                 end
               },
@@ -262,6 +261,31 @@ require("lazy").setup({
         }, {
           { name = 'cmdline' }
         })
+      })
+    end
+  },
+  { "scalameta/nvim-metals",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local metals_config = require("metals").bare_config()
+      metals_config.init_options.statusBarProvider = "on"
+      metals_config.settings = {
+        showImplicitArguments = true,
+        showInferredType = true,
+      }
+      -- Example if you are using cmp how to make sure the correct capabilities for snippets are set
+      metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+      -- Autocmd that will actually be in charging of starting the whole thing
+      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        -- NOTE: You may or may not want java included here. You will need it if you
+        -- want basic Java support but it may also conflict if you are using
+        -- something like nvim-jdtls which also works on a java filetype autocmd.
+        pattern = { "scala", "sbt", "java" },
+        callback = function()
+          require("metals").initialize_or_attach(metals_config)
+        end,
+        group = nvim_metals_group,
       })
     end
   }

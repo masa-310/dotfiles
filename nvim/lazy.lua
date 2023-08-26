d8219cb97d21d2010ca8148d3e154630b3293df1
Masashi Sato <gmasa.310@gmail.com>
2023-08-26 22:15:33 +0900

    a

--- nvim/lazy.lua
+++ nvim/lazy.lua
@@ -89,6 +89,7 @@ require("lazy").setup({
         vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
         vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
         vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+        vim.diagnostic.setqflist()
 
         -- Use LspAttach autocommand to only map the following keys
         -- after the language server attaches to the current buffer

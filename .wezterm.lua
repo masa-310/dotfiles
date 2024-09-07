24026c36b32e7788d65661ab453ded820f753210
Masashi SATO <gmasa.310@gmail.com>
2024-09-07 11:34:58 +0900

    update transparency

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,12 +7,12 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 config.color_scheme = "Cobalt Neon"
 -- config.color_scheme = "Molokai"
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.80
 config.front_end = "WebGpu"
 config.enable_wayland = false
 config.font = wezterm.font("Inconsolata Nerd Font Mono")
@@ -22,45 +22,45 @@ config.hide_tab_bar_if_only_one_tab = true
 -- timeout_milliseconds defaults to 1000 and can be omitted
 config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
-  {
-    key = "%",
-    mods = "LEADER|SHIFT",
-    action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-  },
-  {
-    key = "&",
-    mods = "LEADER|SHIFT",
-    action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-  },
-  {
-    key = "t",
-    mods = "LEADER",
-    action = wezterm.action.SpawnTab("CurrentPaneDomain"),
-  },
-  {
-    key = "n",
-    mods = "LEADER",
-    action = wezterm.action.ActivateTabRelative(1),
-  },
-  {
-    key = "p",
-    mods = "LEADER",
-    action = wezterm.action.ActivateTabRelative(-1),
-  },
-  -- Send "CTRL-B" to the terminal when pressing CTRL-B, CTRL-B
-  {
-    key = "b",
-    mods = "LEADER|CTRL",
-    action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
-  },
+	{
+		key = "%",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "&",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "t",
+		mods = "LEADER",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		key = "n",
+		mods = "LEADER",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		key = "p",
+		mods = "LEADER",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	-- Send "CTRL-B" to the terminal when pressing CTRL-B, CTRL-B
+	{
+		key = "b",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
+	},
 }
 
 for i = 1, 9 do
-  table.insert(config.keys, {
-    key = tostring(i),
-    mods = "LEADER",
-    action = wezterm.action.ActivateTab(i - 1),
-  })
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = wezterm.action.ActivateTab(i - 1),
+	})
 end
 
 -- and finally, return the configuration to wezterm

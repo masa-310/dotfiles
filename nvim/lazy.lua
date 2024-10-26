3798609237b5a2e91731855fe1758854bc7e7146
Masashi SATO <gmasa.310@gmail.com>
2024-10-26 19:51:18 +0900

    update

--- nvim/lazy.lua
+++ nvim/lazy.lua
@@ -201,20 +201,20 @@ require("lazy").setup({
       local cspell = require("cspell")
 
       local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-      -- local on_attach = function(client, bufnr)
-      --   if client.supports_method("textDocument/formatting") then
-      --     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      --     vim.api.nvim_create_autocmd("BufWritePre", {
-      --       group = augroup,
-      --       buffer = bufnr,
-      --       callback = function()
-      --         -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-      --         -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-      --         vim.lsp.buf.formatting_sync()
-      --       end,
-      --     })
-      --   end
-      -- end
+      local on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+              -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+              vim.lsp.buf.formatting_sync()
+            end,
+          })
+        end
+      end
       -- local on_attach = function(client)
       --   require("lsp-format").on_attach(client)
       -- end
@@ -248,7 +248,7 @@ require("lazy").setup({
           cspell.code_actions,
         },
 
-        -- on_attach = on_attach,
+        on_attach = on_attach,
       })
     end,
   },

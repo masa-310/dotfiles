6014eae285bda2cef66af09d62532826417c9f9e
Masashi SATO <gmasa.310@gmail.com>
2024-10-26 19:23:36 +0900

    update

--- nvim/lazy.lua
+++ nvim/lazy.lua
@@ -200,9 +200,20 @@ require("lazy").setup({
       local null_ls = require("null-ls")
       local cspell = require("cspell")
 
-      require("lsp-format").setup({})
-      local on_attach = function(client)
-        require("lsp-format").on_attach(client)
+      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+      local on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+              -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+              vim.lsp.buf.formatting_sync()
+            end,
+          })
+        end
       end
 
       null_ls.setup({

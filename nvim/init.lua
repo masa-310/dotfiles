23744a63aabf210e43d9ed524cc9eb92fe335111
Masashi SATO <gmasa.310@gmail.com>
2025-05-06 14:42:01 +0900

    update kickstart

--- nvim/init.lua
+++ nvim/init.lua
@@ -647,84 +647,105 @@ require('lazy').setup({
         },
       }
 
+
       -- LSP servers and clients are able to communicate to each other what features they support.
       --  By default, Neovim doesn't support everything that is in the LSP specification.
       --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
       --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
       local capabilities = require('blink.cmp').get_lsp_capabilities()
 
-      -- Enable the following language servers
-      --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-      --
-      --  Add any additional override configuration in the following tables. Available keys are:
-      --  - cmd (table): Override the default command used to start the server
-      --  - filetypes (table): Override the default list of associated filetypes for the server
-      --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-      --  - settings (table): Override the default settings passed when initializing the server.
-      --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-      local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
+      -- ================== own config ===================================================
+			local lspconfig = require("lspconfig")
 
-        lua_ls = {
-          -- cmd = { ... },
-          -- filetypes = { ... },
-          -- capabilities = {},
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
-            },
-          },
-        },
-      }
+lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+				init_options = {
+					documentFormatting = false,
+				},
+				flags = {
+					debounce_text_changes = 300,
+				},
+			})
+			lspconfig.eslint.setup({
+				capabilities = capabilities,
+				init_options = {
+					documentFormatting = false,
+				},
+				flags = {
+					debounce_text_changes = 300,
+				},
+			})
+			lspconfig.tailwindcss.setup({
+				capabilities = capabilities,
+				settings = {
+					tailwindCSS = {
+						experimental = {
+							classRegex = {
+								{ "tv\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+							},
+						},
+					},
+				},
+				flags = {
+					debounce_text_changes = 500,
+				},
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
 
-      -- Ensure the servers and tools above are installed
-      --
-      -- To check the current status of installed tools and/or manually install
-      -- other tools, you can run
-      --    :Mason
-      --
-      -- You can press `g?` for help in this menu.
-      --
-      -- `mason` had to be setup earlier: to configure its options see the
-      -- `dependencies` table for `nvim-lspconfig` above.
-      --
-      -- You can add other tools here that you want Mason to install
-      -- for you, so that they are available from within Neovim.
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
-      })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.elmls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.hls.setup({
+				filetypes = { "haskell", "lhaskell", "cabal" },
+				capabilities = capabilities,
+			})
+			lspconfig.ccls.setup({
+				filetypes = { "c", "cpp" },
+				capabilities = capabilities,
+			})
+			lspconfig.purescriptls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.buf_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.metals.setup({
+				capabilities = capabilities,
+				filetypes = { "scala", "sbt" },
+			})
+			lspconfig.sqls.setup({
+				cmd = { "sqls" },
+				on_attach = function(client, bufnr)
+					require("sqls").on_attach(client, bufnr)
+					local bufmap = vim.api.nvim_buf_set_keymap
+					bufmap(bufnr, "n", "<LocalLeader>ss", "<cmd>SqlsExecuteQuery<CR>", { silent = true })
+					bufmap(bufnr, "v", "<LocalLeader>ss", "<cmd>SqlsExecuteQuery<CR>", { silent = true })
+				end,
+				capabilities = capabilities,
+				single_file_support = false,
+				on_new_config = function(new_config, new_rootdir)
+					new_config.cmd = {
+						"sqls",
+						"-config",
+						new_rootdir .. "/.sqls.yml",
+					}
+				end,
+			})
 
-      require('mason-lspconfig').setup {
-        ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-        automatic_installation = false,
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
-      }
     end,
   },
 

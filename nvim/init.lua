591b7b785d34c83876b752b5df4e908f5d554237
Masashi SATO <gmasa.310@gmail.com>
2025-05-06 16:58:34 +0900

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -852,14 +852,7 @@ require('lazy').setup({
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
-        ["<CR>"] = {
-          function(cmp)
-            local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-            return cmp.accept({
-              callback = cmp_autopairs.on_confirm_done
-            })
-          end
-        }
+        ["<CR>"] = { 'select_accept_and_enter', 'fallback'}
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
       },

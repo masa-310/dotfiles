18223841225788c90cbd6078aee08f8f0098ec95
Masashi SATO <gmasa.310@gmail.com>
2025-09-10 21:10:31 +0900

    🐛 fix(nvim): add prettier formatting for typescriptreact

--- nvim/init.lua
+++ nvim/init.lua
@@ -801,6 +801,7 @@ require('lazy').setup({
         typescript = { "prettierd", "prettier", stop_after_first = true },
         tsx = { "prettierd", "prettier", stop_after_first = true },
         javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+        typescriptreact = { "prettierd", "prettier", stop_after_first = true },
       },
     },
   },

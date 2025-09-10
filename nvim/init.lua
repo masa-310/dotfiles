e463a787b139f6329c6c4a3616f65bf95b9e9ff8
Masashi SATO <gmasa.310@gmail.com>
2025-09-10 21:03:38 +0900

    ✨ feat(format): add formatters for go and typescript/react
    Configures `conform.nvim` to use `goimports`/`gofmt` for Go and `prettierd` for TypeScript and React files.

--- nvim/init.lua
+++ nvim/init.lua
@@ -792,11 +792,15 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        go = { "goimports", "gofmt"},
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
+        tsx = { "prettierd", "prettier", stop_after_first = true },
+        javascriptreact = { "prettierd", "prettier", stop_after_first = true },
       },
     },
   },
